b9fd40631849ba6235b79fe85203c82f607f7ded
junne.wanja@student.moringaschool.com <junne.wanja@student.moringaschool.com>
2024-07-11 13:27:02 +0300

    function lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const hqlocation = 42;
+    return Math.abs(someValue - hqlocation);
+  }
+  
+  function distanceFromHqInFeet(someValue) {
+    const feetPerBlock = 264
+    const distanceInBlocks = distanceFromHqInBlocks(someValue)
+    return distanceInBlocks*feetPerBlock
+  }
+  function distanceTravelledInFeet(start,destination) {
+    const blockDistance= (start - destination)
+    const feetPerBlock=264
+    return Math.abs(blockDistance*feetPerBlock)
+  }
+  function calculatesFarePrice(start,destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+      return 0
+    }
+    else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+      return Math.abs((distanceTravelledInFeet(start, destination)-400) * 0.02)
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+      return 25
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2500) {
+      return 'cannot travel that far'
+    }
+  }
+  
